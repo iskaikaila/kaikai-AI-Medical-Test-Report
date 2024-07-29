79daa455112b9a1451c95d8f756d8d6cf2fcda28
yongh27 <yongh9129@gmail.com>
2024-07-29 11:03:41 +0800

    Update App.js

--- aimedicalreport/src/App.js
+++ aimedicalreport/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import  React  from  'react';
+import  {  BrowserRouter  as  Router,  Routes,  Route  }  from  'react-router-dom';
+import  Login  from  './pages/Login';
+import  RegistrationForm  from  './pages/RegistrationForm';
+import  Home  from  './pages/Home'; 
+import  SearchFilter  from  './components/SearchFilter';
+import  EditRecord  from  './components/EditRecord';
+import  DownloadRecord  from  './components/DownloadRecord';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const  App  =  ()  =>  {
+    return  (
+      <Router>
+        <Routes>
+          <Route  path="/"  element={<Home  />}  />  {/*  主页路由  */}
+          <Route  path="/login"  element={<Login  />}  />
+          <Route  path="/register"  element={<RegistrationForm  />}  />
+          <Route  path="/edit-record/:id"  component={EditRecord}  />
+          <Route  path="/download-record/:id"  component={DownloadRecord}  />
+        </Routes>
+      </Router>
+    );
+};
 
-export default App;
+export  default  App;
+
